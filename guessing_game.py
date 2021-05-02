908fc2c64f47cf4c2030e8b1d2e4c16d4e2be9b7
Ashley Ball <me@ashleyball.com.au>
2021-05-02 15:49:21 +1000

    first commit

---
+++ guessing_game.py
@@ -0,0 +1,25 @@
+# Guessing Game - Automate The Boring Stuff with Python Programming Course
+
+import random
+
+print("Hi there, what\'s your name?")
+name = input("Your name: ")
+
+print ("Hi " + name + ", I\'m thinking of a number between 1 - 6, what is it? (You have six guesses)")
+
+x = random.randint(1,10) # Set random number to guess
+limit = 6 # Set guess limit to six
+
+# Detect if higher or lower
+for limit in range(1,7):
+    guess = int(input())
+    if guess < x:
+        print("That's lower.")
+    elif guess > x:
+        print("That's higher.")
+    else: break # Stop once correct guess
+
+if guess == x: # Display win/loss message
+    print("Correct! You got it! Nice work " + name + ", the number is " + str(x))
+else:
+    print("Better luck next time, " + name + ", the random number is " + str(x))
